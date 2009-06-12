3fe50c5bce2972639a5f0fdf00cd664ab012ed20
Oliver Thomas <oliver@lernresistenz.de>
2009-06-12 20:56:41 +0200

    kann jetzt boegen drehen

---
+++ helfer/bogendrehen.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+# bogen richtig drehen, ueberschreibt urspruengliche dateien
+# haette gern two-pages-tifs ...
+# USAGE: bogendrehen.rb file1.tif file2.tif ...
+
+require 'RMagick'
+include Magick
+
+def find_barcode_on_first(imagelist, tmp_filename)
+  imagelist[0].write(tmp_filename)
+  r = `bardecode #{tmp_filename}`
+  if not r.empty?
+    return r.strip.sub(/^.*\(/,'').sub(')]','').split(',').map{ |s|
+      s.to_i } 
+  else
+    return nil
+  end
+end
+
+ARGV.each do |filename|
+  pages = ImageList.new(filename)
+
+  tmp_filename = 'tmp.tif'
+  r = find_barcode_on_first(pages, filename)
+  
+  if r.nil?
+    r = find_barcode_on_first(pages.reverse!, filename)
+  end
+
+  if r[0] < r[1]
+    pages = ImageList.new(filename).map! { |i| i.rotate(180) }
+  end
+
+  pages.write(filename)
+end
