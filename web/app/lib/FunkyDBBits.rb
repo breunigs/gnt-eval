4c7a85b5184f6bf719c4de8250324ac9a6fc6100
Oliver Thomas <oliver@mathphys.fsk.uni-heidelberg.de>
2009-09-20 19:39:52 +0200

    refactoring \o/

--- web/app/lib/FunkyDBBits.rb
+++ web/app/lib/FunkyDBBits.rb
@@ -4,26 +4,38 @@
 module FunkyDBBits
   attr :dbh, :db_table
   
-  # returns count of stuff where i IN h[i] for each i + additional
-  def count_forms(h, additional = '')
-    b = 0
-    q = "SELECT COUNT(*) AS anzahl FROM #{@db_table} WHERE " +
-        "  #{fold_where_clause(h)} " + additional
+  # query fields, where-hash and additional clauses
+  def query(f, h, additional = '')
+    q = 'SELECT '
+    q += f.to_a.join(', ')
+    q += " FROM #{@db_table}"
+    q += ' WHERE ' + h.keys.join(' IN (?) AND ') + ' IN (?) ' +
+      additional
+    
+    result = nil
     sth = @dbh.prepare(q)
     sth.execute(*h.values)
-    sth.fetch_hash { |r| b = r['anzahl'] }
-    return b
+    sth.fetch_array { |r| result = r }
+    if result.count == 1
+      return result[0]
+    else
+      return result
+    end
   end
   
-  
+  # returns count of stuff where i IN h[i] for each i + additional
+  def count_forms(h, additional = '')
+    return query('COUNT(*)', h, additional)
+  end
+
   # (where-clause-)hash h, question q
   def multi_q(h, q)
     anzahl = count_forms(h, " AND (#{q.db_column.join('+')} > 0)")
     answers = Hash.new
     q.db_column.each_index do |i|
       c = q.db_column[i]
-      answers[q.boxes[i].text] = ( count_forms(h, " AND #{c} >
-                                   0").to_f * 100 / anzahl.to_f + 0.5
+      answers[q.boxes[i].text] = ( count_forms(h, " AND #{c} > " +
+                                   '0').to_f * 100 / anzahl.to_f + 0.5
                                  ).to_i   
     end
     
@@ -34,24 +46,12 @@ module FunkyDBBits
   # h_general: where-clause-hash für die vergleichsveranstaltungen
   def single_q(h_particular, h_general, q)
     col = q.db_column
-    b_q = "SELECT STD(#{col}) AS sigma, AVG(#{col}) AS mittel, " +
-          "              COUNT(#{col}) AS anzahl " +
-          "          FROM #{@db_table} " +
-          "      WHERE #{col} != 0 AND "
+    mittel, sigma, anzahl = query(["STD(#{col})", "AVG(#{col})",
+                                   "COUNT(#{col})"], h_particular,
+                                  "AND #{col} != 0") 
+    mittel_alle, sigma_alle = query(["STD(#{col})", "AVG(#{col})"],
+                                   h_general, "AND #{col} != 0")
 
-    mittel, sigma, anzahl = 0
-    
-    @dbh.prepare(b_q + fold_where_clause(h_particular)) do |sth|
-      sth.execute(*h_particular.values)
-      sth.fetch { |r| sigma, mittel, anzahl = r }
-    end
-          
-    mittel_alle, sigma_alle = 0
-    @dbh.prepare(b_q + fold_where_clause(h_general)) do |sth|
-      sth.execute(*h_general.values)
-      sth.fetch { |r| sigma_alle, mittel_alle = r[0], r[1] }
-    end
-    
     # single values
     antworten = Array.new
     (1..q.size).each do |i|
@@ -60,10 +60,4 @@ module FunkyDBBits
 
     return [antworten, anzahl, mittel, mittel_alle, sigma, sigma_alle]
   end
-  
-  # folding for use in 'i IN h[i] AND ...'
-  def fold_where_clause(hash)
-    hash.keys.join(' IN (?) AND ') + ' IN (?)'
-  end
-
 end
