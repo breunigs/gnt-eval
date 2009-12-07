d46a554c90e711598fb44062607e493e88cf622c
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2009-12-07 20:12:40 +0100

    Added new bits to Form.rb required for updated OMR

--- web/app/lib/Form.rb
+++ web/app/lib/Form.rb
@@ -12,18 +12,22 @@
 
 # This is a box on a printed form. Nothing more.
 class Box
-  
+
   # value to insert into database
   attr_accessor :coiche
 
   # coordinates
   attr_accessor :x,:y
-  
+
   # size
   attr_accessor :width, :height
 
   # what is the _meaning_ of this box
   attr_accessor :text
+
+  # special value for easier LaTeX sheet generation
+  attr_accessor :type
+
 
   def initialize(c, x, y, w, h, t)
     @choice = c
@@ -32,40 +36,41 @@ class Box
     @width = w
     @height = h
     @text = t
+    @type = ""
   end
 end
 
 # This is a question on a printed form. Nothing more.
-class Question 
+class Question
   include FunkyDBBits
 
   # list of boxes
   attr_accessor :boxes
-  
+
   # text of the question
   attr_accessor :qtext
-  
+
   # value to insert into database if OMR fails
   attr_accessor :failchoice
-  
+
   # value to insert into db if there is no mark
   attr_accessor :nochoice
-  
+
   # what does the box look like (defaults to square)
   attr_accessor :type
-  
+
   # into which field to write the result (use a list for multiple choice questions!)
   attr_accessor :db_column
-  
+
   # active question? (defaults to true)
   attr_accessor :active
-  
+
   # postfix when saving file
   attr_accessor :save_as
-  
+
   # belongs to: 'tutor', 'prof', 'tutoring'
   attr_accessor :section
-  
+
   def initialize(boxes = [], qtext='', failchoice=-1,
                  nochoice=nil, type='square', db_column='',
                  active=true, save_as = '', section = '')
@@ -93,7 +98,7 @@ class Question
     else
       return false
     end
-  end 
+  end
 
   # for compatibility reasons
   def saveas
@@ -109,36 +114,36 @@ class Question
   def rtext
     @boxes.last.text
   end
-  
+
   # question itself
   def text
     @qtext
   end
-  
+
   def eval_to_tex(this_eval, bc, db_table, dbh)
     @dbh = dbh
     @db_table = db_table
-    
+
     b = ''
-    
+
     if @db_column.is_a?(Array)
-        
+
       answers = multi_q({ :eval => this_eval, :barcode =>
                           bc}, self)
-      
+
       t = TeXMultiQuestion.new(@qtext, answers)
       b << t.to_tex
-      
+
       # single-q
     else
       antw, anz, m, m_a, s, s_a = single_q({:eval => this_eval,
                                              :barcode =>
                                              bc},
-                                           {:eval => this_eval}, self) 
-      
+                                           {:eval => this_eval}, self)
+
       t = TeXSingleQuestion.new(text, ltext, rtext, antw,
                                 anz, m, m_a, s, s_a)
-      
+
       b << t.to_tex
     end
     return b
@@ -147,7 +152,7 @@ end
 
 
 # this is actually just needed for the OMR to distinguish between
-# pages 
+# pages
 #
 
 class Page
@@ -165,10 +170,10 @@ end
 #
 
 class Form
-  
+
   # list of pages
   attr_accessor :pages
-  
+
   # database table to use for this form
   attr_accessor :db_table
 
@@ -182,3 +187,4 @@ class Form
     @pages.collect { |p| p.questions }.flatten
   end
 end
+
