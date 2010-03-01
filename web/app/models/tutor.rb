b6ce5a3eb0266cba1c91a4de40b6294fb106948e
Oliver Thomas <oliver@mathphys.fsk.uni-heidelberg.de>
2010-03-01 22:26:05 +0100

    methods wie competence etc. hinzugefügt. das sollte vielleicht automagisch geschehen, indem über eine form automatisch die methoden hinzugefügt werden. sollte eigentlich nicht so viel arbeit sein. ... future

--- web/app/models/tutor.rb
+++ web/app/models/tutor.rb
@@ -8,11 +8,8 @@ class Tutor < ActiveRecord::Base
   def eval_against_form(form, dbh)
     @dbh = dbh
     @db_table = form.db_table
-    boegenanzahl = count_forms({:barcode => course.course_profs.map{ |cp| cp.barcode.to_i},
-                                :tutnum => tutnum}) 
 
-
-    return '', nil unless boegenanzahl > 2
+    return '', nil unless boegenanzahl(@dbh) > 2
     b = ''
     b << "\\section{#{abbr_name}}\n\n"
     
@@ -34,5 +31,41 @@ class Tutor < ActiveRecord::Base
   
   def tutnum
     course.tutors.sort{ |x,y| x.id <=> y.id }.index(self) + 1
+  end
+  
+  def competence(dbh)
+    @dbh = dbh
+    @db_table = course.form.to_form.db_table
+    competence_field = 'AVG(t2)'
+    query(competence_field, { :barcode => course.course_profs.map{ |cp| cp.barcode.to_i}, :tutnum => tutnum}, " AND t2 > 0").to_f
+  end
+  
+  def profit(dbh)
+    @dbh = dbh
+    @db_table = course.form.to_form.db_table
+    profit_field = 'AVG(t10)'
+    query(profit_field, { :barcode => course.course_profs.map{ |cp| cp.barcode.to_i}, :tutnum => tutnum}, " AND t10 > 0").to_f
+    
+  end
+  
+  def teacher(dbh)
+    @dbh = dbh
+    @db_table = course.form.to_form.db_table
+    teacher_field = 'AVG(t1)'
+    query(teacher_field, { :barcode => course.course_profs.map{ |cp| cp.barcode.to_i}, :tutnum => tutnum}, " AND t1 > 0").to_f
+    
+  end
+  def preparation(dbh)
+    @dbh = dbh
+    @db_table = course.form.to_form.db_table
+    prep_field = 'AVG(t3)'
+    query(prep_field, { :barcode => course.course_profs.map{ |cp| cp.barcode.to_i}, :tutnum => tutnum}, " AND t3 > 0").to_f
+    
+  end
+  def boegenanzahl(dbh)
+    @dbh = dbh
+    @db_table = course.form.to_form.db_table
+    count_forms({:barcode => course.course_profs.map{ |cp| cp.barcode.to_i},
+                                :tutnum => tutnum}) 
   end
 end
