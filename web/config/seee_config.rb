a46e53b4fe75b68e1fafe452dde4fd395dcf72f4
Oliver Thomas <oliver@lernresistenz.de>
2010-07-27 17:24:30 +0200

    und jetzt auch Config am richtigen Ort

--- web/config/seee_config.rb
+++ web/config/seee_config.rb
@@ -1,12 +1,1 @@
-#!/usr/bin/env ruby
-
-require 'active_support'
-
-module Seee
-  module Config
-    mattr_accessor :application_paths
-    @@application_paths = {
-      :hunspell => '/usr/bin/env hunspell'
-    }
-  end
-end
+../lib/seee_config.rb
