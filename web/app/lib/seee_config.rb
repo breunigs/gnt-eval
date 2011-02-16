f44416d4c41f9fdb724fde4c2af61b1ded774c65
xeen <stefan@mathphys.fsk.uni-heidelberg.de>
2011-02-16 22:27:46 +0100

    translate comments in seee_config (fixes #255)

--- web/app/lib/seee_config.rb
+++ web/app/lib/seee_config.rb
@@ -8,49 +8,49 @@ module Seee
   module Config
     mattr_accessor :application_paths, :file_paths, :commands, :external_database, :settings, :custom_builds
 
-    # Pfade für die custom-ImageMagick/RMagick Builds
+    # paths for custom-ImageMagick/RMagick builds
     @@custom_builds = {
-      # Unterordner, in dem alles gespeichert wird
+      # subfolder to save all build-related stuff to
       :dir => File.join(RAILS_ROOT, "..", "custom_build"),
     }
     @@custom_builds.merge!({
-      # Pfade zum source code. Das * verhindert, dass man bei jedem
-      # Versionsupdate die Config ändern muss
+      # paths to the sources. The * saves us from having to change the
+      # config on upstream updates
       :src_img_magick => File.join(@@custom_builds[:dir], "ImageMagick*"),
       :src_r_magick => File.join(@@custom_builds[:dir], "RMagick*"),
       :src_zbar => File.join(@@custom_builds[:dir], "zbarimg_hackup/zbar-0.10"),
 
-      # Pfade, wohin die Programme installiert werden. Sollte nicht mit
-      # dem * von oben matchen
+      # defines the paths where the builds should be installed to. This should
+      # not match on the * from above.
       :bld_img_magick => File.join(@@custom_builds[:dir], "build_imagemagick"),
       :bld_r_magick => File.join(@@custom_builds[:dir], "build_rmagick"),
       :bld_zbar => File.join(@@custom_builds[:dir], "build_zbar"),
     })
     @@custom_builds.merge!({
-      # Findet den Pfad der custom RMagick.rb, wenn diese installiert ist,
-      # ansonsten nil. Eigentlich keine Einstellung, aber trotzdem ein guter
-      # Ort um sie aufzubewahren.
+      # contains the path to the custom RMagick.rb if it has been built. It's
+      # nil otherwise. While this is not actually a pref, it seems to be in a
+      # good place.
       :rmagick_rb => Dir.glob(File.join(@@custom_builds[:bld_r_magick], "**", "RMagick.rb"))[0],
-      # siehe auch @@file_paths[:rmagick]
+      # see also: @@file_paths[:rmagick]
     })
 
-    # Die sollten recht selbsterklärend sein
+    # these pretty much speak for themselves
     @@application_paths = {
       :hunspell => '/usr/bin/hunspell',
       :pdflatex => '/home/jasper/texlive/2009/bin/x86_64-linux/pdflatex',
-      # das ist das alte zbar, das die Standard ImageMagick Pfade nutzt
+      # this is the old zbar, which uses the default ImageMagick
       :zbar_shared => File.join(RAILS_ROOT, '..', 'helfer', 'zbarimg'),
-      # das ist das zbar, das gegen das eigene ImageMagick gelinkt wurde
+      # this is the custom zbar, which uses the custom ImageMagick
       :zbar_cust => File.join(@@custom_builds[:bld_zbar], "bin", "zbarimg")
     }
     @@application_paths.merge!({
-      # Nutze das eigene zbar wenn möglich
+      # prefer own zbar over the default one
       :zbar => File.exists?(@@application_paths[:zbar_cust]) \
             ? @@application_paths[:zbar_cust]                \
             : @@application_paths[:zbar_shared],
     })
 
-    # Externe Datenbank, in der die wirklichen Evaldaten gespeichert sind
+    # external database (i.e. not the Rails one) which stores the real eval data
     @@external_database = {
       :dbi_handler => 'Mysql',
       :username => 'eval',
@@ -59,7 +59,7 @@ module Seee
     }
 
     @@settings = {
-      # FQDN des standard-empfängers
+      # default recipient's FQDN
       :standard_mail_domain => 'mathphys.fsk.uni-heidelberg.de',
       :standard_mail_from => 'evaluation@mathphys.fsk.uni-heidelberg.de',
       :standard_mail_bcc => 'evaluation@mathphys.fsk.uni-heidelberg.de',
@@ -73,8 +73,8 @@ module Seee
       # Specify a directory where to cache things
       :cache_tmp_dir => '/tmp/seee/',
 
-      # Verzeichnis, in dem die Kommentarbilder gespeichert
-      # werden. Hat dann pro Semester Unterordner.
+      # directory to store the extracted comment images to. Each semester
+      # has its own subfolder.
       :comment_images_public_dir => '/home/eval/public_html/.comments/',
 
       # Same directory as above, but available via http
@@ -89,13 +89,14 @@ module Seee
 
       :hunspell_personal_dic => File.join(Rails.root, 'app/lib/persdic.dic'),
 
-      # Benutze die angepasste RMagick Version wenn möglich, ansonsten
-      # falle auf die globale Version zurück (gedacht für "require")
+      # prefer the custom RMagick version over the default one. Its meant
+      # to be used for "require" and should automatically fall back to
+      # default if custom build is not available.
       :rmagick => @@custom_builds[:rmagick_rb] || "RMagick"
     }
 
 
-    # Sind Pfade nicht gesetzt, defaulte auf /usr/bin/env
+    # Ask /usr/bin/env for unconfigured/unspecified paths
     def @@application_paths.default(key=nil)
       if not key.nil?
         "/usr/bin/env #{key.to_s}"
@@ -104,8 +105,8 @@ module Seee
       end
     end
 
-    # Spezielle Kommandos, die ggf. rechtespezifisch sind, also nicht
-    # nur application_paths.
+    # special commands which might be access right depended. I.e. anything
+    # that is more than an “application_path”
 
     @@commands = {
       :find_comment_image_directory => 'login_gruppe_home eval find',
@@ -128,8 +129,8 @@ module Seee
       :zbar => @@application_paths[:zbar].to_s + ' --set ean13.disable=1 --set upce.disable=1 --set isbn10.disable=1 --set upca.disable=1 --set isbn13.disable=1 --set i25.disable=1 --set code39.disable=1 --set code128.disable=1 --set y-density=4 '
     })
 
-    # Sind Kommandos nicht näher spezifiziert, versuchen wir es
-    # einfach mit dem Applikationspfad
+    # fall back to application_paths if a command
+    # hasn't been configured/is unspecified
     def @@commands.default(key=nil)
       @@application_paths[key]
     end
