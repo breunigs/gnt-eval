0df78a92596a74fe05c22c94b024751d0edc7363
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2011-03-30 17:36:41 +0200

    forgot for last commit

--- web/app/lib/seee_config.rb
+++ web/app/lib/seee_config.rb
@@ -39,7 +39,7 @@ module Seee
       :hunspell => '/usr/bin/hunspell',
       :pdflatex => '/home/jasper/texlive/2009/bin/x86_64-linux/pdflatex',
       # this is the old zbar, which uses the default ImageMagick
-      :zbar_shared => File.join(RAILS_ROOT, '..', 'helfer', 'zbarimg'),
+      :zbar_shared => File.join(RAILS_ROOT, '..', 'helfer', "zbarimg_#{`uname -m`}"),
       # this is the custom zbar, which uses the custom ImageMagick
       :zbar_cust => File.join(@@custom_builds[:bld_zbar], "bin", "zbarimg")
     }
