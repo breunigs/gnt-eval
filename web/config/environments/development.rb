698082bf946cf47dd9516f189fb77345e52ae306
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2011-06-03 17:32:06 +0200

    disable caching and add some more requires in a desperate move to fix the darn thing

--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -12,7 +12,7 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 # enable this, even in development to be able to catch stale caches
-config.action_controller.perform_caching             = true
+config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -2,14 +2,14 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-config.cache_classes = true
+config.cache_classes = false
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
+config.action_controller.perform_caching             = false
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
