3c23afff8d7b1c8d8fb082e6d5d6bb3e1de72028
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2011-06-06 17:27:18 +0200

    add basic scan script for our lovely scanner

---
+++ helfer/scan.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# start scanning
+if [ "$1" = "" ]; then
+	scanadf \
+		--buffermode=yes \
+		--stapledetect=yes \
+		--rollerdeskew=yes \
+		--swdeskew=yes \
+		--df-length=yes \
+		--df-thickness=yes \
+		-x 210 \
+		-y 297 \
+		--page-width 210 \
+		--page-height 297 \
+		--source 'ADF Duplex' \
+		--mode lineart \
+		--resolution 300 \
+		-o 'foo_%d.pnm' \
+		-S "$0"
+	exit 0
+fi
+
+nn=$(echo $1 | sed -r 's/.*\_([0-9]+).*/\1/')
+n=$(( $nn - 1 ))
+
+
+pnmtotiff "foo_${nn}.pnm" > "foo_${nn}.tiff"
+rm "${1}"
+
+rem=$(( $nn % 2 ))
+
+if [ $rem -eq 0 ]; then
+	newid=$(( $nn / 2 ))
+	convert "foo_${n}.tiff" "foo_${nn}.tiff" -adjoin "scanned_${newid}.tiff"
+	rm "foo_${n}.tiff" "foo_${nn}.tiff"
+fi
+
