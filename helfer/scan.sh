89ec98da23776c8a84fd9a56262207de9e9ab6d0
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2011-07-04 17:43:08 +0200

    add support for continuos scanning

--- helfer/scan.sh
+++ helfer/scan.sh
@@ -2,22 +2,28 @@
 
 # start scanning
 if [ "$1" = "" ]; then
-	scanadf \
-		--buffermode=yes \
-		--stapledetect=yes \
-		--rollerdeskew=yes \
-		--df-length=yes \
-		--df-thickness=yes \
-		-x 210 \
-		-y 297 \
-		--page-width 210 \
-		--page-height 297 \
-		--source 'ADF Duplex' \
-		--mode lineart \
-		--resolution 300 \
-		-o 'foo_%d.pnm' \
-		-S "$0"
-	exit 0
+	while  [ 1 ]; do
+		count=$(ls -1 | wc -l)
+		final=$(( $count * 2 + 1 ))
+		scanadf \
+			--buffermode=yes \
+			--stapledetect=yes \
+			--rollerdeskew=yes \
+			--df-length=yes \
+			--df-thickness=yes \
+			-x 210 \
+			-y 297 \
+			--page-width 210 \
+			--page-height 297 \
+			--source 'ADF Duplex' \
+			--mode lineart \
+			--resolution 300 \
+			-o 'foo_%d.pnm' \
+			-S "$0" \
+			--start-count "${final}"
+		echo "Continue? (Press Enter)"
+		read
+	done
 fi
 
 nn=$(echo $1 | sed -r 's/.*\_([0-9]+).*/\1/')
@@ -31,7 +37,8 @@ rem=$(( $nn % 2 ))
 
 if [ $rem -eq 0 ]; then
 	newid=$(( $nn / 2 ))
-	convert "foo_${n}.tif" "foo_${nn}.tif" -adjoin "scanned_${newid}.tif"
+	zeroid=$(printf "%06d" $newid)
+	convert "foo_${n}.tif" "foo_${nn}.tif" -adjoin "scanned_${zeroid}.tif"
 	rm "foo_${n}.tif" "foo_${nn}.tif"
 fi
 
