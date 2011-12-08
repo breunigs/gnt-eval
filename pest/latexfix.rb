88faa5c555437e0c4de4ab5b6c992c651435fbd9
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2011-12-08 15:28:51 +0100

    add some more magic to eval.cls so that the output yamls is fine

--- pest/latexfix.rb
+++ pest/latexfix.rb
@@ -10,6 +10,9 @@
 # indent the YAML file with spaces. Therefore, the indentation is done
 # using leading 'u's instead. This script replaces those with normal
 # spaces so it can be parsed as YAML file.
+#
+# The same can be said about YAML comments which are identified by a
+# leading '#'. Those are expressed by a capital 'U'.
 
 if ARGV.empty?
     puts "Usage: ./latexfix.rb fixme1.yaml [fixme2.yaml, ...]"
@@ -20,12 +23,13 @@ ARGV.each do |file|
     f = File.open(file, "r");
     newfile = []
     f.each do |l|
-        newfile << l.gsub(/^uuu\s/, "  " * 3) \
-                    .gsub(/^uu\s/, "  " * 2)  \
-                    .gsub(/^u\s/, "  ")       \
-                    .gsub(/^uuu-/, ("  " * 3) + "-")  \
-                    .gsub(/^uu-/,  ("  " * 2) + "-")       \
-                    .gsub(/^u-/, ("  ")  + "-")
+        newfile << l.gsub(/^uuu\s/, "  "*3)        \
+                    .gsub(/^uu\s/,  "  "*2)        \
+                    .gsub(/^u\s/,   "  "*1)        \
+                    .gsub(/^uuu-/, ("  "*3) + "-") \
+                    .gsub(/^uu-/,  ("  "*2) + "-") \
+                    .gsub(/^u-/,   ("  "*1) + "-") \
+                    .gsub(/^U/, "#")
     end
     f.close
     f = File.open(file.gsub(/posout$/, "yaml"), "w+")
