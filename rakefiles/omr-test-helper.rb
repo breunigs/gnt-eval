e552f849fd90f1a7e91e22b3b5fdc919a3ac6ec2
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2011-12-09 22:43:07 +0100

    fix several bugs in the debug helper

--- rakefiles/omr-test-helper.rb
+++ rakefiles/omr-test-helper.rb
@@ -1,6 +1,12 @@
 namespace :testhelper do
   Scc = Seee::Config.commands
 
+  # print and execute a command
+  def cmd(line)
+    puts line
+    system line
+  end
+
   desc "debugs the sample sheets as if they were scanned so you can see if OMR is working correctly"
   task :debug_samplesheets, :needs => 'pdf:samplesheets' do |t, a|
     puts "NOTE: Old sample sheets will not be overwritten. If you updated them,"
@@ -11,16 +17,20 @@ namespace :testhelper do
       Dir.glob("*.yaml") do |f|
         f = f.gsub(/\.yaml$/, "")
         next unless File.exist?("#{f}.pdf")
-
-        unless File.exist?("#{f}.tif")
+        # note that the filename for the yaml file and tif file need to be different. Otherwise the
+        # TeX’s-YAML file will be overwritten by the processed-YAML file which contains the pixel
+        # coordinates instead of the original TeX ones. This makes re-processing cumbersome, as the
+        # sample sheets would have to be re-rendered. A proper way to fix this would be if OMR would
+        # store the pixels into different variables. FIXME
+        unless File.exist?("dbg_#{f}.tif")
           puts
           puts "Now converting #{f}.pdf to tif"
-          system("#{Scc[:convert]} -compress LZW -density 300 -colors 2 \"#{f}.pdf\" \"#{f}.tif\"")
+          cmd("#{Scc[:convert]} -density 300 -monochrome \"#{f}.pdf\" \"dbg_#{f}.tif\"")
         end
 
         puts
-        puts "Now running OMR on #{f}.tif"
-        system("../../pest/omr2.rb -t -d -v -s \"#{f}.yaml\" -p . #{f}.tif")
+        puts "Now running OMR on dbg_#{f}.tif"
+        cmd("../../pest/omr2.rb -t -d -v -s \"#{f}.yaml\" -p . dbg_#{f}.tif")
       end
     end
   end
