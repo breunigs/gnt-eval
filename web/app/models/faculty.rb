f9baa92daa2222501ed00ca309a760b8448506dc
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-01-11 12:05:47 +0100

    leaverage Rails’ associations to get a nice list of barcodes fora faculty

--- web/app/models/faculty.rb
+++ web/app/models/faculty.rb
@@ -1,9 +1,15 @@
 class Faculty < ActiveRecord::Base
   has_many :courses
+  has_many :course_profs, :through => :courses
   validates_presence_of :shortname, :longname
 
   # returns true if there are any courses associated with this faculty
   def critical?
     courses.size > 0
+  end
+
+  # returns array of integer-barcodes for all associated CourseProfs
+  def barcodes
+    course_profs.map { |cp| cp.id }
   end
 end
