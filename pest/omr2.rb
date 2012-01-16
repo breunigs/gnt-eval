breunigs/gnt-eval

commit c5a89c193f5bc7cd8cda1019130517c6d3cdfde7

fix omr not storing the checkmarks for «no answer» boxes on multiple choice questions

pest/omr2.rb
@@ -523,13 +523,10 @@ class PESTOmr < PESTDatabaseTools
       next if q.type == "text" || q.type == "text_wholepage"
       next if q.db_column.nil?
 
-      if q.db_column.is_a?(Array)
-        q.db_column.each_with_index do |a, i|
-          # The first answer starts with 1, but i is zero-based.
-          # Therefore add 1 everytime to put the results in the
-          # right columns.
-          vals << (q.value.include?(i+1) ? 1 : 0).to_s
-          keys << a
+      if q.multi?
+        q.boxes.each do |box|
+          vals << (q.value.include?(box.choice) ? box.choice : 0).to_s
+          keys << q.db_column[i]
         end
       else
         vals << (q.value.nil? ? 0 : Integer(q.value)).to_s

commit 685402b4a6a3ace1e0f3e51e983460a2cfc0fc0a

probably fix crash when OMR'ing MultiQuestions (broken in c5a89c193f5bc7cd8cda1019130517c6d3cdfde7)

pest/omr2.rb
@@ -524,7 +524,7 @@ class PESTOmr < PESTDatabaseTools
       next if q.db_column.nil?
 
       if q.multi?
-        q.boxes.each do |box|
+        q.boxes.each_with_index do |box,i|
           vals << (q.value.include?(box.choice) ? box.choice : 0).to_s
           keys << q.db_column[i]
         end