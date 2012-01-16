989cc216584d69a207a338255267b837b8ac8d70
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-01-16 17:32:26 +0100

    add String#capitalize_first… I don’t actually need it, but I don’t have the heart to remove it

--- web/app/lib/RandomUtils.rb
+++ web/app/lib/RandomUtils.rb
@@ -73,6 +73,12 @@ class String
   def find_common_start
     self
   end
+
+  # Capitalizes the first letter of the string and leaves all other
+  # chars alone.
+  def capitalize_first
+    self.slice(0,1).capitalize + self.slice(1..-1)
+  end
 end
 
 # Tries to simplify paths to make them more user-readable. The path will
