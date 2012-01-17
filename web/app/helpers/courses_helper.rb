c2242a20ae828875a803efba90389f24c52ca832
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-01-17 01:41:27 +0100

    fix form/lang selection in new/edit course to change when the semester is selected

--- web/app/helpers/courses_helper.rb
+++ web/app/helpers/courses_helper.rb
@@ -1,6 +1,40 @@
 module CoursesHelper
   include FunkyTeXBits
 
+  # finds all available languages in all forms
+  def all_langs
+    l = []
+    Semester.all.each do |s|
+      s.forms.each { |f| l << f.languages }
+    end
+    l.flatten.uniq
+  end
+
+  # maps given forms to a hash of hashs like:
+  # { form_id => { :name => "some form", :langs => [:de, :en] } }
+  def map_forms_and_langs(forms)
+    h = {}
+    forms.each do |f|
+      h[f.id] = { :name => f.name, :langs => f.languages }
+    end
+    h
+  end
+
+  # maps all semesters to a hashs of hashs like:
+  # { semester_id => { :title => "WS 10/11", :forms => (see above) } }
+  def map_semesters_and_forms
+    sfl = {}
+    Semester.all.each do |s|
+      sfl[s.id] = { :title => s.title, :forms => map_forms_and_langs(s.forms) }
+    end
+    sfl
+  end
+
+  # returns a JSON version of map_semesters_and_forms
+  def json_map_sfl
+    ActiveSupport::JSON.encode(map_semesters_and_forms)
+  end
+
   def render_preview
     texpreview(@course.summary)
   end
