d3764d6d9e118307ac96141bd1014170d656af2f
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-02-09 18:50:49 +0100

    warn if no filenames given

--- tools/identify_pdfs_with_not_2_pages.rb
+++ tools/identify_pdfs_with_not_2_pages.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
+if ARGV.empty?
+  warn "No filenames given."
+  exit 1
+end
+
 ARGV.each do |filename|
-  command = "pdfinfo \"#{filename}\" | grep Pages: | awk '{print $2}'" 
+  command = "pdfinfo \"#{filename}\" | grep Pages: | awk '{print $2}'"
   pages = `#{command}`.to_i
   puts filename unless pages == 2
 end
