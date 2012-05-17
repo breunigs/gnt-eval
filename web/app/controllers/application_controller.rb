700ee7d7ad92076f59bcfc5826a49b7fa8cb4dac
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-05-17 10:51:54 +0200

    use human names for most of Semester; minor fixes

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # returns the human name of the model associated with the current
+  # controller. I18n is applied.
+  def human_name
+    controller_name.classify.constantize.model_name.human
+  end
 end
