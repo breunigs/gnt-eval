c0702dc5f23a9efb43c97590172a9d6ae6c2f756
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-05-19 16:42:41 +0200

    remove obsolete comments

--- web/config/initializers/001_requirements.rb
+++ web/config/initializers/001_requirements.rb
@@ -2,10 +2,8 @@
 
 require "fileutils"
 
-#~ require 'digest/md5' # FIXME: still required. DEPRECATED (?)
 
 cdir = File.dirname(File.realdirpath(__FILE__))
-#~ require(File.join(File.dirname(__FILE__),'../../config/initializers', 'seee_config.rb'))
 require(File.join(cdir, '../../app/lib', 'result_tools.rb'))
 require(File.join(cdir, '../../app/lib', 'AbstractForm.rb'))
 require(File.join(cdir, '../../app/lib', 'FunkyTeXBits.rb'))
