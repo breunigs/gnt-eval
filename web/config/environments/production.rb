2d436b34c45f5097f2c22dc2c056c0be140f7e0e
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-05-25 09:59:31 +0200

    make production be more like testing since only trusted users are meant to use GnT-Eval. This should allow for easier error reports while the software is still this unstable.

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -6,17 +6,23 @@ Seee::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  # even though in production, print full error messages
+  config.consider_all_requests_local       = true
+  # enable full caching
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # set to false to disable Rails's static asset server (Apache or nginx will already do this)
+  # set to true, to also allow when testing production mode via rails server.
+  config.serve_static_assets = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
+  # Don’t compress the assets for now. It takes ages and many of the
+  # large files are already compressed.
+  config.assets.compress = false
+
+  # don't fallback to assets pipeline if a precompiled asset is missing
   config.assets.compile = false
 
   # Generate digests for assets URLs
