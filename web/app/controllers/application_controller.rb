90c9732e8b630e33216a134b141c0f80ae1b18a2
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-06-15 12:54:23 +0200

    announce that all pages use UTF-8 via headers and meta tags.

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -3,6 +3,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # Set UTF-8 header on everything.
+  before_filter :set_utf8_header
+  def set_utf8_header
+    response.headers['Content-type'] = 'text/html; charset=utf-8'
+  end
+
   # returns the human name of the model associated with the current
   # controller. I18n is applied.
   def human_name
