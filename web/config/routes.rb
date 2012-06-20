d085bf5c9c2f97c53063ee53a3fb47c2e84f6394
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-06-20 10:50:29 +0200

    also allow drop prof via GET

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -15,7 +15,7 @@ Seee::Application.routes.draw do
     get "/tutors/:id/preview" => "tutors#preview"
     member do
       post "/add_prof" => "courses#add_prof"
-      post "/drop_prof" => "courses#drop_prof"
+      match "/drop_prof" => "courses#drop_prof"
       get "/preview" => "courses#preview"
       get "/correlate" => "courses#correlate"
     end
