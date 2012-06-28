c9261c8411837106d68d2f42d380ae36dce0c81f
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-06-28 07:06:01 +0200

    port mailers to rails3 format

--- web/app/models/course.rb
+++ web/app/models/course.rb
@@ -97,13 +97,20 @@ class Course < ActiveRecord::Base
 
   # lovely helper function: we want to guess the mail address of
   # evaluators from their name, simply by adding a standand mail
-  # domain to it
+  # domain to it. Returns comma separated list.
   def fs_contact_addresses
+    fs_contact_addresses_array.join(',')
+  end
+
+  # lovely helper function: we want to guess the mail address of
+  # evaluators from their name, simply by adding a standand mail
+  # domain to it. Returns array
+  def fs_contact_addresses_array
     pre_format = fscontact.blank? ? evaluator : fscontact
 
     pre_format.split(',').map do |a|
       (a =~ /@/ ) ? a : a + '@' + SCs[:standard_mail_domain]
-    end.join(',')
+    end
   end
 
   # Same as above, but do not include the default domain. Intended for
