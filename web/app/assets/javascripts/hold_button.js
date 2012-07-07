b43f8a61037b8b74e677e31dd75d94d135bc5dc2
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-07-07 21:11:49 +0200

    add ability to auto-linkify click-and-hold elements.
    Simply add class="hold" and data-url="http://" to an element to redirect
    the user to the given URL if the element is clicked for more than one
    second.

--- web/app/assets/javascripts/hold_button.js
+++ web/app/assets/javascripts/hold_button.js
@@ -25,3 +25,21 @@ function hold_button(elem, seconds, only_once, url) {
   elem.onmouseup = clear;
   elem.onmouseout = clear;
 }
+
+// auto linkify elements with class "hold"
+$(document).ready(function() {
+  $(".hold").each(function(ind, elem) {
+    if(!$(elem).data("url"))
+      return;
+
+    var t;
+    elem.onclick = function (event) { event.preventDefault(); }
+    var exe = function() {
+      window.location = $(elem).data("url");
+    }
+    elem.onmousedown = function() {
+      if(t) clearTimeout(t);
+      t = setTimeout(exe, 1000);
+    }
+  });
+});
