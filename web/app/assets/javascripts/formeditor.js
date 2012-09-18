c62839f9ef49519073d068c8c115241d29bae8b6
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-09-18 18:21:19 +0200

    implement preview for form editor

--- web/app/assets/javascripts/formeditor.js
+++ web/app/assets/javascripts/formeditor.js
@@ -100,6 +100,48 @@ FormEditor.prototype.updateSaveButton = function(state) {
   }
 };
 
+FormEditor.prototype.updatePreviewButton = function(state) {
+  if(state) {
+    $("#preview").removeClass("disabled").html("Preview");
+  } else {
+    $("#preview").addClass("disabled").html("Previewing…");
+  }
+};
+
+FormEditor.prototype.preview = function() {
+  if($("#preview").hasClass("disabled")) return;
+  this.updatePreviewButton(false);
+
+  if($formHasBeenEdited > 0 && !confirm("Preview requires the form to be saved first. Save form?"))
+    return;
+
+  var f = $("#form_content").parents("form");
+  f.one('ajax:success ajax:error', function(event, b, status, c) {
+    if(status != "success" && !event.isTrigger) {
+      $F().warn("Status is " + status + ", aborting preview.");
+      return;
+    }
+
+    $("#form_preview").one("click", function() {
+      $F().updatePreviewButton(true);
+      $("#form_preview").fadeOut("fast", function() {
+        $("#form_preview").html("");
+      });
+      $("#darkened").fadeOut();
+    });
+
+    $("#form_preview").load(ATTRIBUTES["PreviewUrl"], function() {
+      $("#form_preview").fadeIn();
+      $("#darkened").fadeIn();
+    });
+  });
+
+  if($formHasBeenEdited > 0)
+    this.save();
+  else
+    f.trigger("ajax:success");
+};
+
 FormEditor.prototype.toggleSorting = function(enable) {
   enable = enable === undefined || enable === null ? $("#sort").is(":visible") : enable;
   if(enable) {
