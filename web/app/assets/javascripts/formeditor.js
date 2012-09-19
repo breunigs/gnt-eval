b8f54e8aabe1e20c099ca28bd114d259555b7e40
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-09-19 08:54:56 +0200

    ensure init js file is loaded first for form editor

--- web/app/assets/javascripts/formeditor.js
+++ web/app/assets/javascripts/formeditor.js
@@ -1,4 +1,5 @@
 // The formeditor is split up for better overview, but the different
 // files depend on each other. The order of inclusion does not matter.
 
+//= require ./formeditor/init-and-misc.js
 //= require_tree ./formeditor
