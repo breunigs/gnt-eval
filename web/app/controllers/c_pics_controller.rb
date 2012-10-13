b0d5bdfc066214df60de459b246fd3632991639e
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2012-10-13 13:40:40 +0200

    fix showing original sheets for courses; improve error message

--- web/app/controllers/c_pics_controller.rb
+++ web/app/controllers/c_pics_controller.rb
@@ -3,7 +3,7 @@
 class CPicsController < ApplicationController
   def download
     @cpic = CPic.find(params[:id])
-    raise ActionController::RoutingError.new('Not Found') unless @cpic && File.exists?(@cpic.source)
+    raise ActionController::RoutingError.new("Original Sheet Could not be found. Looked in #{@cpic.source}") unless @cpic && File.exists?(@cpic.source)
     send_file(@cpic.source)
   end
 end
