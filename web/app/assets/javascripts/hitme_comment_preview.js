53c5d00bd1b8ef72dea6e0ea7df170f8a2513e3a
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-01-18 04:49:23 +0100

    implement split layout for all hitmes.
    Previously only used for the inputs/comments in final check, it
    is now used for the job description and metadata as well as
    the input/preview on typing, proofreading and combining. Also,
    all pages now autosize the comment field.
    
    This should allow for a better usage of current screens, as long
    as about 1000px in width are available.

--- web/app/assets/javascripts/hitme_comment_preview.js
+++ web/app/assets/javascripts/hitme_comment_preview.js
@@ -1,32 +1,44 @@
-var renderInProgress = false;
-var renderTimeout = null;
-var edit = null;
+var renderTimeout = {};
+var renderInProgress = {};
 if(typeof listify === 'undefined') var listify = false;
 
-function renderPreview() {
-  renderInProgress = true;
-  $("#rendermsg").html("Rendering…");
+function renderPreview(id) {
+  renderInProgress[id] = true;
 
-  $("#previewbox").load(
+  var edit = $("#"+id).data("editor");
+
+  var con = $("#"+id).parent().find(".previewbox");
+  con.children(".rendermsg").html("Rendering…");
+
+  var d = con.children("div");
+  d.css('height', d.height());
+  d.load(
     hitme_preview_url,
     { "text": edit.getValue(), "listify": listify },
     function() {
-      renderInProgress = false;
-      $("#rendermsg").html('<a onclick="renderPreview()">Force Update now</a>');
+      // http://stackoverflow.com/a/4086085/1684530
+      $(this).wrapInner('<div/>');
+      $(this).animate({height: $('div:first', this).height()} );
+      con.children(".rendermsg").html('<a onclick="renderPreview(\''+id+'\')">Force Update now</a>');
+      renderInProgress[id] = false;
     }
   );
 }
 
-$(document).ready(function() {
-  edit = $("#text").data("editor");
-  if(!edit) return;
-  sess = edit.getSession();
 
-  sess.on('change', function(){
-    if(renderTimeout) clearTimeout(renderTimeout);
-    if(renderInProgress) return;
-    renderTimeout = setTimeout("renderPreview()", 1000);
+$(document).ready(function() {
+  $(".containersplit #text, textarea.previewable").each(function(ind, txt) {
+    var txt = $(txt);
+    var id = txt.attr("id");
+
+    txt.data("editor").getSession().on('change', function(){
+      var rt = renderTimeout[id];
+      if(rt) clearTimeout(rt);
+      if(renderInProgress[id]) return;
+      renderTimeout[id] = setTimeout("renderPreview('"+id+"')", 1000);
+    });
+
+    renderTimeout[id] = setTimeout("renderPreview('"+id+"')", 400);
   });
 
-  renderPreview();
 });
