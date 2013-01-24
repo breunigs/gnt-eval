d58bc23484130ce22b49a688acadc44f0ab3e883
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-01-24 09:27:59 +0100

    consolidate hold_button

--- web/app/assets/javascripts/hold_button.js
+++ web/app/assets/javascripts/hold_button.js
@@ -1,53 +1,39 @@
-// long press button to submit associated form.
-// Use via the shared/_hold_button view.
-function hold_button(elem, seconds, only_once, url) {
-  var t;
-
-  var exe = function() {
-    if(!confirm("Continue?")) return;
-
-    if(only_once) {
-      elem.mousedown = function () { return false; }
-      elem.setAttribute("disabled", "disabled");
-    }
-    // set URL here to avoid non-javascript browsers from submitting
-    // the button too easily
-    elem.form.setAttribute("action", url);
-    elem.form.submit();
-  }
-
-  elem.removeAttribute("disabled");
-
-  elem.onclick = function (event) { event.preventDefault(); }
-
-  elem.onmousedown = elem.ontouchstart = function() {
-    if(t) clearTimeout(t);
-    t = setTimeout(exe, seconds*1000);
-  }
-
-  var clear = function () { clearTimeout(t); }
-  elem.onmouseup = clear;
-  elem.onmouseout = clear;
-  elem.ontouchend = clear;
-}
-
-
 // auto linkify elements with class "hold". Put the URL to
 // redirect to in a data-url attribute.
+// Other options:
+// data-time -- seconds the button needs to be held
+// data-post -- fill with authenticity_token to submit via POST instead
+// data-onlyonce -- disable button after submission
+// data-group -- if onlyonce is specified, all buttons in this group
+//               will be disabled
+// See web/app/views/shared/_hold_button.html.erb for an example
 $(document).ready(function() {
 
   $(".hold").each(function(ind, elem) {
-    if(!$(elem).data("url"))
+    var e = $(elem);
+    if(!e.data("url") || e.hasClass("disabled"))
       return;
 
     var t;
     elem.onclick = function (event) { event.preventDefault(); }
     var exe = function() {
-      window.location = $(elem).data("url");
+      if(e.data("onlyonce")) {
+        $("[data-group='"+e.data("group")+"']").addClass("disabled");
+        e.addClass("disabled");
+      }
+
+      if(e.data("post")) {
+        $('body').append($('<form/>')
+        .attr({'action': e.data("url"), 'method': 'post', "id": "post_location"})
+        .append($('<input/>').attr({'type': 'hidden', 'name': 'authenticity_token', 'value': e.data("post")})
+        )).find("#post_location").submit();
+      } else {
+        window.location = e.data("url");
+      }
     }
     elem.onmousedown = elem.touchstart = function() {
       if(t) clearTimeout(t);
-      t = setTimeout(exe, 1000);
+      t = setTimeout(exe, 1000*(e.data("time") || 1 ));
     }
     var clear = function () { clearTimeout(t); }
     elem.onmouseup = clear;
